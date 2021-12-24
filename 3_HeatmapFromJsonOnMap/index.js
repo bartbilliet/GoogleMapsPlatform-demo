964c1ecdc921ae02b0548abd60b2eb7bcf9efd93
Bart Billiet <bartbilliet@gmail.com>
2021-12-24 19:23:33 +0100

    Heatmap sample earthquake data

---
+++ 3_HeatmapFromJsonOnMap/index.js
@@ -0,0 +1,50 @@
+let map;
+
+function initMap() {
+  const brussels = { lat: 50.8476, lng: 4.3572 };
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 3,
+    center: brussels,
+    mapTypeId: "terrain",
+  });
+
+  // Create a <script> tag and set the USGS URL as the source.
+  const script = document.createElement("script");
+
+  
+  // Loading past 7-day M2.5+ earthquake data from earthquake.usgs.gov (via CORS)
+  // For details about the dataset, see: https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+
+  // When the script runs, the target domain passes the data as an argument to another script, usually named callback(). In our example the callback function name is eqfeed_callback. 
+  // The target domain defines the callback script name, which is the first name on the page when you load the target URL in a browser.
+  script.src = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp";
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+// We define the callback script here in our code, here named eqfeed_callback based on the callback script name defined by our GeoJSON data script. 
+// Loop through the results array and place a marker for each set of coordinates received from the above script. 
+const eqfeed_callback = function (results) {
+  const heatmapData = [];
+
+  for (let i = 0; i < results.features.length; i++) {
+    //Coordinates of the earthquake
+    const coords = results.features[i].geometry.coordinates;
+    const latLng = new google.maps.LatLng(coords[1], coords[0]);
+
+    //Weight of the earthquake on scale of Richter
+    const mag = results.features[i].properties.mag;
+
+    //Push the coordinates + magnitude weight to an array
+    heatmapData.push( { location: latLng, weight: mag } );
+  }
+
+  //Display heatmap layer from previously filled up array.
+  const heatmap = new google.maps.visualization.HeatmapLayer({
+    data: heatmapData,
+    dissipating: false,
+    map: map,
+  });
+
+};
+
--- README.md
+++ README.md
@@ -8,3 +8,6 @@ Small Hello World by adding a *hardcoded* marker to a Google Maps, embedded in a
 Map out [usgs.gov earthquake data](https://www.usgs.gov/programs/earthquake-hazards/earthquakes) as markers on Google Maps (in [GeoJSON format](https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php)). 
 The data shows all M2.5+ earthquakes in the last 7 days worldwide. 
 
+3. **[Weighted heatmap from external GeoJSON data on map](.\3_HeatmapFromJsonOnMap)**<br />
+Heatmap from [usgs.gov earthquake data](https://www.usgs.gov/programs/earthquake-hazards/earthquakes), where earthquake magnitude is represented as weights on Google Maps (in [GeoJSON format](https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php)). 
+The data shows all M2.5+ earthquakes in the last 7 days worldwide. 
